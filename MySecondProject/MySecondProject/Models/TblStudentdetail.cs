a9f36b8fe33772239784246f69a898053c96a8e3
Nehith Mateti <nehith.m@gmail.com>
2022-05-10 09:50:01 +0530

    My First check in

---
+++ MySecondProject/MySecondProject/Models/TblStudentdetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySecondProject.Models
+{
+    public partial class TblStudentdetail
+    {
+        public long Sno { get; set; }
+        public long SId { get; set; }
+        public string? SFirstname { get; set; }
+        public string? SLastname { get; set; }
+        public int? SAge { get; set; }
+        public string? SGender { get; set; }
+        public string? SCourse { get; set; }
+        public string? SAddress { get; set; }
+        public long? SPhone { get; set; }
+        public DateTime? Dob { get; set; }
+        public DateTime? DateOfJoining { get; set; }
+    }
+}
---
+++ MySecondProject/MySecondProject/Models/TblStudentmark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySecondProject.Models
+{
+    public partial class TblStudentmark
+    {
+        public int Serialno { get; set; }
+        public long StudentId { get; set; }
+        public decimal? SFeespaid { get; set; }
+        public int? SMathsmarks { get; set; }
+        public int? SPhysicsmarks { get; set; }
+        public int? SChemistrymarks { get; set; }
+        public int? SSciencemarks { get; set; }
+        public int? SEnglishmarks { get; set; }
+        public int? STotalmarks { get; set; }
+        public double? SPercentage { get; set; }
+
+        public virtual TblStudentdetail Student { get; set; } = null!;
+    }
+}
